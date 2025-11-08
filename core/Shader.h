57b33fd849d24c5c10e63fc937d7eca2ed7e3890
hemithermos <julien76michaud@gmail.com>
2025-11-08 01:11:09 +0100

    added the Shader helper class

---
+++ core/Shader.h
@@ -0,0 +1,26 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader
+{
+public:
+    // shader id
+    unsigned int ID;
+
+    // constructor to read the shaders
+    Shader(const char *vertexShaderPath, const char *fragShaderPath);
+    // activate the shader using glUseProgram
+    void use();
+    // utils uniform functions
+    void setBool(const std::string &name, bool value) const;
+    void setInt(const std::string &name, int value) const;
+    void setFloat(const std::string &name, float value) const;
+};
+
+#endif
